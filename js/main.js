f8ddbaaae07cdf7060fe527068e76b0813ff3a73
PipZip <37911683+PipZip@users.noreply.github.com>
2019-05-16 23:29:54 +0600

    serts update

--- js/main.js
+++ js/main.js
@@ -45,11 +45,42 @@ $(function() {
 
     $('[data-fancybox="images"]').fancybox({
         infobar: false,
-        toolbar: false,
         transitionEffect: 'slide',
         baseClass: "images-fancy",
+        buttons: [
+            "close"
+        ],
+        btnTpl: {
+            close:
+                '<button data-fancybox-close class="fancybox-button fancybox-button--close">' +
+                '<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26.4561 1L1.00021 26.4558" stroke="white" stroke-width="2"/><path d="M26.4561 26.4561L1.00021 1.00021" stroke="white" stroke-width="2"/></svg>' +
+                "</button>",
+            arrowLeft:
+                '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left">' +
+                '<div><svg width="72" height="30" viewBox="0 0 72 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.585785 16.4142C-0.195259 15.6332 -0.195259 14.3668 0.585785 13.5858L13.3137 0.857864C14.0948 0.0768156 15.3611 0.0768156 16.1421 0.857864C16.9232 1.63891 16.9232 2.90524 16.1421 3.68629L4.82843 15L16.1421 26.3137C16.9232 27.0948 16.9232 28.3611 16.1421 29.1421C15.3611 29.9232 14.0948 29.9232 13.3137 29.1421L0.585785 16.4142ZM72 17H2V13H72V17Z" fill="white"/></svg></div>' +
+                "</button>",
+
+            arrowRight:
+                '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right">' +
+                '<div><svg width="72" height="30" viewBox="0 0 72 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M71.4142 16.4142C72.1953 15.6332 72.1953 14.3668 71.4142 13.5858L58.6863 0.857864C57.9052 0.0768156 56.6389 0.0768156 55.8579 0.857864C55.0768 1.63891 55.0768 2.90524 55.8579 3.68629L67.1716 15L55.8579 26.3137C55.0768 27.0948 55.0768 28.3611 55.8579 29.1421C56.6389 29.9232 57.9052 29.9232 58.6863 29.1421L71.4142 16.4142ZM0 17H70V13H0V17Z" fill="#FFFFFF"/></svg></div>' +
+                "</button>",
+
+        },
+        thumbs : {
+            autoStart : true,
+            axis      : 'x'
+        },
+        afterLoad : function() {
+            $('.fancybox-thumbs__list a').each(function() {
+                number = $(this).data('index') + 1;
+                myNum = addZero(number);
+                $(this).html(myNum);
+            });
+        }
     });
 });
+
+
 
 function noRobot(e) {
     var range = $(e).val();
@@ -130,7 +161,8 @@ var swiperSerts = new Swiper('.serts-swiper .swiper-container', {
 
 
 function addZero(n) {
-    return n.length > 1 ? n : (+n > 0) ? "0" + n : n;
+    var x = (typeof n!=='string') ? String(n) : n;
+    return x.length > 1 ? n : (+n > 0) ? "0" + n : n;
 }
 
 
